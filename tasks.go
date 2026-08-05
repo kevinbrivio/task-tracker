17936211ecab3de6828e889feb54ddfc14585923
kevinbriv <kevin.brivio@helios.id>
2026-08-05 21:53:23 +0700

    add(Task): task model

---
+++ tasks.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+
+type Status string
+
+const (
+	StatusTodo       Status = "todo"
+	StatusInProgress Status = "in_progress"
+	StatusDone       Status = "done"
+)
+
+type Task struct {
+	ID          string    `json:"id"`
+	Description string    `json:"description"`
+	Status      Status    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
